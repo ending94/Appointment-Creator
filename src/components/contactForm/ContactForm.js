e94c238d5ab3970bdfe95891bfb6cbcc82a4147d
Czarek <ggmanutd0@gmail.com>
2022-09-02 13:23:32 +0100

    updated contact form

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -7,9 +7,42 @@ export const ContactForm = ({
   setPhone,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    ContactForm
+    <form onSubmit={handleSubmit}>
+      <label>
+        <input
+          type="text"
+          name="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Contact Name"
+          required
+        />
+      </label>
+      <label>
+        <input
+          type="tel"
+          value={phone}
+          name="phone"
+          onChange={(e) => setPhone(e.target.value)}
+          placeholder="XXX-XXX-XXXX"
+          pattern="[1-9][0-9]{2}-[1-9][0-9]{2}-[0-9]{4}"
+          required
+        />
+      </label>
+      <label>
+        <input
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Contact Email"
+          required
+        />
+      </label>
+      <input type="submit" value="Add Contact" />
+    </form>
   );
 };
